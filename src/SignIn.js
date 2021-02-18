044ac5f3c86b5ba40708cac05aa1cce7bc92c519
Andrii1996 <a.medzyak@gmail.com>
2021-02-18 16:24:23 +0200

    add submit function

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,30 +1,13 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import React, { useState } from 'react';
+import {Button,
+  CssBaseline,
+  Link,
+  TextField,
+  Grid,
+  Typography,
+  Container
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -44,23 +27,66 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  logo: {
+    width: '849px',
+    height: '900px',
+    background: 'red',
+  },
+  img: {
+    // backgroundImage: URL('')
+  },
+  main: {
+    display: 'flex',
+  },
 }));
 
 export default function SignIn() {
   const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailErr, setEmailErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+
+  const handleEmail = (event) => {
+    setEmailErr('');
+    setEmail(event.target.value)
+  }
+
+  const handlePassword = (event) => {
+    setPasswordErr('');
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!email) {
+      setEmailErr('Email required')
+      return;
+    }
+
+    if (!password) {
+      setPasswordErr('Email password')
+      return;
+    }
+
+    alert(`Email - ${email}\nPassword - ${password}`);
+  }
 
   return (
-    <Container component="main" maxWidth="xs">
+    <div className={classes.main}>
+      <div className={classes.logo}>
+        <img className={classes.img} src="./img/logo.png" alt="main logo"/>
+      </div>
+      <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
           <TextField
+            onChange={handleEmail}
             variant="outlined"
             margin="normal"
             required
@@ -71,7 +97,9 @@ export default function SignIn() {
             autoComplete="email"
             autoFocus
           />
+          <p>{emailErr}</p>
           <TextField
+            onChange={handlePassword}
             variant="outlined"
             margin="normal"
             required
@@ -82,10 +110,7 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+          <p>{passwordErr}</p>
           <Button
             type="submit"
             fullWidth
@@ -109,9 +134,7 @@ export default function SignIn() {
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
     </Container>
+    </div>
   );
 }
